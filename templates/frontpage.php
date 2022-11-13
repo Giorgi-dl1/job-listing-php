ed16ffeec3ded3356a3442f315f38292c55c1300
Giorgi-dl1 <96057077+Giorgi-dl1@users.noreply.github.com>
2022-11-14 01:46:47 +0400

    implement homepage

--- templates/frontpage.php
+++ templates/frontpage.php
@@ -1,2 +1,31 @@
-<?php 
-echo $title;
+<?php include './includes/header.php' ?> 
+
+
+    <div class="p-[2rem] bg-slate-200 mt-[2rem] text-center">
+        <h1 class="text-5xl">heading</h1>
+        <p class="mt-5">text</p>
+        <button class="mt-5 bg-red-500 text-white p-3 rounded hover:opacity-80 ease duration-500">Sign up today</button>
+    </div>
+
+    <div class="flex flex-col gap-4">
+        <div class="flex justify-between">
+            <div>
+                <div class="text-lg font-semibold">heading</div>
+                <div>desc</div>
+            </div>
+            <div>
+                 <button class="mt-5 max-h-max bg-green-500 gap-4 text-white px-3 py-2 rounded hover:opacity-80 ease duration-500">View</button>
+            </div>
+                   </div>
+        <div class="flex justify-between">
+            <div>
+                <div class="text-lg font-semibold">heading</div>
+                <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto tenetur ratione animi unde, autem a at aperiam suscipit quaerat dolor praesentium consequuntur cumque. Similique aliquam delectus molestiae odit animi laborum.</div>
+            </div>
+            <div>
+                <button class="mt-5 max-h-max bg-green-500 gap-4 text-white px-3 py-2 rounded hover:opacity-80 ease duration-500">View</button>
+            </div>
+        </div>
+    </div>
+<?php include './includes/footer.php' ?> 
+
---
+++ templates/includes/header.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+  <?php include 'output.css' ?>
+</style>
+<body>
+<main class="lg:px-[8rem] md:px-[5rem] px-[0]">
+    <nav class="flex justify-between py-5 border-b-[1px] border-[hsla(0,0,0,0.4)]">
+        <h3 class="text-2xl tracking-wider opacity-60">VACANCIES</h3>
+        <ul class="flex justify-between gap-[1rem] text-red-700">
+            <li class="cursor-pointer hover:opacity-50 ease duration-500">
+                <a href="index.php">
+                    Home 
+                </a>
+            </li>
+            <li class="cursor-pointer hover:opacity-50 ease duration-500">
+            <a href="create.php">Create Vacany</a>
+            </li>
+        </ul>
+    </nav>
